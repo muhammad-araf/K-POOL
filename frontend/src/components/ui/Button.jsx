d9c911b454f5d6ec8bf875fce71db4a3945298ad
muhammad-araf <lentoaraf@gmail.com>
2025-12-26 19:17:44 +0500

    Changes Done

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Loader2 } from 'lucide-react';
-import { clsx } from 'clsx';
+// import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 const Button = ({
